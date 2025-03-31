a4fc6d399967902176bd4112e4c0093f0ad4c4ef
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-31 06:17:09 +0300

    added model to admin

--- repairs/admin.py
+++ repairs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import RepairRequest
 
-# Register your models here.
+@admin.register(RepairRequest)
+class RepairRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'device_type', 'created_at', 'is_processed')
+    list_filter = ('device_type', 'is_processed', 'created_at')
+    search_fields = ('name', 'phone', 'description')
