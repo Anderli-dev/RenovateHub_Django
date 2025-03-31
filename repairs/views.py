6f65458cf613e9f6ec66b528f06c0e64e4278983
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-31 06:28:13 +0300

    added basic logic with home page

--- repairs/views.py
+++ repairs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.http import HttpResponse
+from repairs.forms import RepairRequestForm
 
-# Create your views here.
+class HomeView(FormView):
+    template_name = 'home.html'
+    form_class = RepairRequestForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.save()
+        if self.request.htmx:
+            return HttpResponse('<div class="alert alert-success">Заявку прийнято!</div>')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        if self.request.htmx:
+            return HttpResponse('<div class="alert alert-danger">Форма містить помилки.</div>')
+        return super().form_invalid(form)
