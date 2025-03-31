79b934089e2ba57e9a22a613a33a74540d7031af
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-31 06:52:11 +0300

    added compact admin panel for comfirm request

--- repairs/urls.py
+++ repairs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from repairs.views import HomeView
+from repairs.views import HomeView, RepairRequestListView, MarkProcessedView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('requests/', RepairRequestListView.as_view(), name='request_list'),
+    path('requests/<int:pk>/processed/', MarkProcessedView.as_view(), name='mark_processed'),
 ]
--- repairs/views.py
+++ repairs/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import get_object_or_404
+from django.views import View
+from django.views.generic import ListView
 from django.views.generic.edit import FormView
-from django.http import HttpResponse
+
 from repairs.forms import RepairRequestForm
+
+from repairs.models import RepairRequest
+
 
 class HomeView(FormView):
     template_name = 'home.html'
@@ -17,3 +24,20 @@ class HomeView(FormView):
         if self.request.htmx:
             return HttpResponse('<div class="alert alert-danger">Форма містить помилки.</div>')
         return super().form_invalid(form)
+
+class RepairRequestListView(ListView):
+    model = RepairRequest
+    template_name = 'request_list.html'
+    context_object_name = 'requests'
+    ordering = ['-created_at']
+
+
+class MarkProcessedView(View):
+    def post(self, request, pk):
+        repair_request = get_object_or_404(RepairRequest, pk=pk)
+        repair_request.is_processed = True
+        repair_request.save()
+        return HttpResponse('<span class="badge bg-success">Опрацьовано</span>')
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponseNotAllowed(['POST'])
